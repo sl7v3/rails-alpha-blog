e550d345971158a59eeeeecde0d10893669f7436
Sulai <seun.lasisi@yahoo.com>
2016-05-17 15:01:54 +0000

    Editing and updating article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -10,6 +14,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render :new
+    end
+  end
+  
+  def update
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
     end
   end
   
