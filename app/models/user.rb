5eed80a9d1a2f177043b6a59e583c4f310b5c03e
Sulai <seun.lasisi@yahoo.com>
2016-05-20 05:17:35 +0000

    Added secure password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 105 },
 						format: { with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
+	has_secure_password
 end
