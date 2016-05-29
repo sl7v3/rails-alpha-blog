0d010b5e09dd500a92cb123c77a54d519d0e1dab
Sulai <seun.lasisi@yahoo.com>
2016-05-29 23:01:28 +0000

    Require user to view links from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   def new
     @user  = User.new
@@ -51,5 +52,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   
   end
+  
+  def require_same_user
+
+    if current_user != @user
+    
+      flash[:danger] = "You can only edit your own account"
+      
+      redirect_to root_path
+    
+    end
+  
+  end
+  
   
 end
