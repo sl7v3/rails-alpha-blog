3fe51beee8f771d159a1cadd3f43d59a59b32092
Sulai <seun.lasisi@yahoo.com>
2016-05-29 22:05:56 +0000

    Login UI, helper files and redundancy cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'pages/about', to: 'pages#about'  
-  get 'pages/home', to: 'pages#home'  
+  get 'pages/home', to: 'pages#home'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :articles
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
