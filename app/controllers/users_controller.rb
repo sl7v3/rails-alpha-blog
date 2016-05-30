83155fd67f4beeb5779ab558a70d4ae63771f48d
Sulai <seun.lasisi@yahoo.com>
2016-05-30 17:27:18 +0000

    Added admin privileges and Admin display in UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   
   def require_same_user
 
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
     
       flash[:danger] = "You can only edit your own account"
       
